303f993a1f4deac2b3f5928d0626c2bb7b4a65f7
weizhiao <1207410841@qq.com>
2024-10-31 17:38:44 +0800

    implement plic

--- drivers/plic/src/lib.rs
+++ drivers/plic/src/lib.rs
@@ -1,125 +1,118 @@
 #![no_std]
-#[allow(clippy::upper_case_acronyms)]
+const INTR_PRIORITY_BASE: usize = 0x000000;
+const INTR_ENABLE_BASE: usize = 0x002000;
+const PRIORITY_THRESHOLD_BASE: usize = 0x200000;
+const INTR_CLAIM_BASE: usize = 0x200004;
+
 pub struct PLIC {
     base_addr: usize,
 }
 
 #[derive(Copy, Clone)]
-pub enum IntrTargetPriority {
+pub enum TargetPriority {
     Machine = 0,
     Supervisor = 1,
 }
 
-impl IntrTargetPriority {
+impl TargetPriority {
     pub fn supported_number() -> usize {
         2
     }
 }
 
 impl PLIC {
-    fn priority_ptr(&self, intr_source_id: usize) -> *mut u32 {
-        assert!(intr_source_id > 0 && intr_source_id <= 132);
-        (self.base_addr + intr_source_id * 4) as *mut u32
+    fn intr_priority_ptr(&self, intr_id: usize) -> *mut u32 {
+        (self.base_addr + INTR_PRIORITY_BASE + intr_id * 4) as *mut u32
     }
-    fn hart_id_with_priority(hart_id: usize, target_priority: IntrTargetPriority) -> usize {
-        let priority_num = IntrTargetPriority::supported_number();
+    fn intr_target(hart_id: usize, target_priority: TargetPriority) -> usize {
+        let priority_num = TargetPriority::supported_number();
         hart_id * priority_num + target_priority as usize
     }
-    fn enable_ptr(
+    fn intr_enable_ptr(
         &self,
         hart_id: usize,
-        target_priority: IntrTargetPriority,
-        intr_source_id: usize,
+        target_priority: TargetPriority,
+        intr_id: usize,
     ) -> (*mut u32, usize) {
-        let id = Self::hart_id_with_priority(hart_id, target_priority);
-        let (reg_id, reg_shift) = (intr_source_id / 32, intr_source_id % 32);
+        let id = Self::intr_target(hart_id, target_priority);
+        let (reg_id, reg_shift) = (intr_id / 32, intr_id % 32);
         (
-            (self.base_addr + 0x2000 + 0x80 * id + 0x4 * reg_id) as *mut u32,
+            (self.base_addr + INTR_ENABLE_BASE + 0x80 * id + 0x4 * reg_id) as *mut u32,
             reg_shift,
         )
     }
-    fn threshold_ptr_of_hart_with_priority(
-        &self,
-        hart_id: usize,
-        target_priority: IntrTargetPriority,
-    ) -> *mut u32 {
-        let id = Self::hart_id_with_priority(hart_id, target_priority);
-        (self.base_addr + 0x20_0000 + 0x1000 * id) as *mut u32
+    fn priority_threshold_ptr(&self, hart_id: usize, target_priority: TargetPriority) -> *mut u32 {
+        let id = Self::intr_target(hart_id, target_priority);
+        (self.base_addr + PRIORITY_THRESHOLD_BASE + 0x1000 * id) as *mut u32
     }
-    fn claim_comp_ptr_of_hart_with_priority(
-        &self,
-        hart_id: usize,
-        target_priority: IntrTargetPriority,
-    ) -> *mut u32 {
-        let id = Self::hart_id_with_priority(hart_id, target_priority);
-        (self.base_addr + 0x20_0004 + 0x1000 * id) as *mut u32
+    fn intr_claim_ptr(&self, hart_id: usize, target_priority: TargetPriority) -> *mut u32 {
+        let id = Self::intr_target(hart_id, target_priority);
+        (self.base_addr + INTR_CLAIM_BASE + 0x1000 * id) as *mut u32
     }
     pub unsafe fn new(base_addr: usize) -> Self {
         Self { base_addr }
     }
-    pub fn set_priority(&mut self, intr_source_id: usize, priority: u32) {
-        assert!(priority < 8);
+    /// The interrupt priority for each interrupt source.
+    pub fn set_intr_priority(&mut self, intr_id: usize, priority: u32) {
         unsafe {
-            self.priority_ptr(intr_source_id).write_volatile(priority);
+            self.intr_priority_ptr(intr_id).write_volatile(priority);
         }
     }
-    #[allow(unused)]
-    pub fn get_priority(&mut self, intr_source_id: usize) -> u32 {
-        unsafe { self.priority_ptr(intr_source_id).read_volatile() & 7 }
+    /// The interrupt priority for each interrupt source.
+    pub fn get_intr_priority(&mut self, intr_id: usize) -> u32 {
+        unsafe { self.intr_priority_ptr(intr_id).read_volatile() }
     }
-    pub fn enable(
+    /// The enablement of interrupt source of each context.
+    pub fn set_intr_enable(
         &mut self,
         hart_id: usize,
-        target_priority: IntrTargetPriority,
-        intr_source_id: usize,
+        target_priority: TargetPriority,
+        intr_id: usize,
     ) {
-        let (reg_ptr, shift) = self.enable_ptr(hart_id, target_priority, intr_source_id);
+        let (ptr, shift) = self.intr_enable_ptr(hart_id, target_priority, intr_id);
         unsafe {
-            reg_ptr.write_volatile(reg_ptr.read_volatile() | 1 << shift);
+            ptr.write_volatile(ptr.read_volatile() | 1u32 << shift);
         }
     }
-    #[allow(unused)]
-    pub fn disable(
+    /// The enablement of interrupt source of each context.
+    pub fn set_intr_disable(
         &mut self,
         hart_id: usize,
-        target_priority: IntrTargetPriority,
-        intr_source_id: usize,
+        target_priority: TargetPriority,
+        intr_id: usize,
     ) {
-        let (reg_ptr, shift) = self.enable_ptr(hart_id, target_priority, intr_source_id);
+        let (ptr, shift) = self.intr_enable_ptr(hart_id, target_priority, intr_id);
         unsafe {
-            reg_ptr.write_volatile(reg_ptr.read_volatile() & (!(1u32 << shift)));
+            ptr.write_volatile(ptr.read_volatile() & (!(1u32 << shift)));
         }
     }
-    pub fn set_threshold(
+    /// The interrupt priority threshold of each context.
+    pub fn set_target_threshold(
         &mut self,
         hart_id: usize,
-        target_priority: IntrTargetPriority,
+        target_priority: TargetPriority,
         threshold: u32,
     ) {
-        assert!(threshold < 8);
-        let threshold_ptr = self.threshold_ptr_of_hart_with_priority(hart_id, target_priority);
+        let threshold_ptr = self.priority_threshold_ptr(hart_id, target_priority);
         unsafe {
             threshold_ptr.write_volatile(threshold);
         }
     }
-    #[allow(unused)]
-    pub fn get_threshold(&mut self, hart_id: usize, target_priority: IntrTargetPriority) -> u32 {
-        let threshold_ptr = self.threshold_ptr_of_hart_with_priority(hart_id, target_priority);
-        unsafe { threshold_ptr.read_volatile() & 7 }
+    /// The interrupt priority threshold of each context.
+    pub fn get_target_threshold(&mut self, hart_id: usize, target_priority: TargetPriority) -> u32 {
+        let threshold_ptr = self.priority_threshold_ptr(hart_id, target_priority);
+        unsafe { threshold_ptr.read_volatile() }
     }
-    pub fn claim(&mut self, hart_id: usize, target_priority: IntrTargetPriority) -> u32 {
-        let claim_comp_ptr = self.claim_comp_ptr_of_hart_with_priority(hart_id, target_priority);
-        unsafe { claim_comp_ptr.read_volatile() }
+    /// The register to acquire interrupt source ID of each context.
+    pub fn claim(&mut self, hart_id: usize, target_priority: TargetPriority) -> u32 {
+        let claim_ptr = self.intr_claim_ptr(hart_id, target_priority);
+        unsafe { claim_ptr.read_volatile() }
     }
-    pub fn complete(
-        &mut self,
-        hart_id: usize,
-        target_priority: IntrTargetPriority,
-        completion: u32,
-    ) {
-        let claim_comp_ptr = self.claim_comp_ptr_of_hart_with_priority(hart_id, target_priority);
+    /// The register to send interrupt completion message to the associated gateway.
+    pub fn complete(&mut self, hart_id: usize, target_priority: TargetPriority, intr_id: u32) {
+        let comp_ptr = self.intr_claim_ptr(hart_id, target_priority);
         unsafe {
-            claim_comp_ptr.write_volatile(completion);
+            comp_ptr.write_volatile(intr_id);
         }
     }
 }
